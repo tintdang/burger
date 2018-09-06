1615b68489fbe109e60d698b5feef1c215e3161e
TDang <tin2dang@gmail.com>
2018-09-06 15:42:55 -0500

    added basic files for server

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,25 @@
 var connection = require("./connection.js")
 
-var orm = {}
+var orm = {
+    selectAll: function(table, cb){
+        var queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function(table, col, val, cb){
+        var queryString = `INSERT INTO ${table} (${col.toString()})VALUES (${/* insert quesiton marks ehre*/d})";`
+        connection.query(queryString, val, function(err, result){
+            if (err) throw err;
+
+            cb(result);
+        })
+    },
+    updateOne: function(table, obj, condition, cb){
+        var queryString = `UPDATE ${table} SET ${/**/(obj)} WHERE ${condition}`
+        
+    }
+}
 
 module.exports = orm;
